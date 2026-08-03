78a58e1c7584a30d25f6fa4f9a97e3bf3f74bd64
Vimal Raval <ravalvimal2008@gmail.com>
2026-08-03 14:04:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (20.90%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def divisorGame(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n % 2 == 0:
+            return True
+        else:
+            return False 
