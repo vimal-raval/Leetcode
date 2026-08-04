c2be86487a29e245ab46f95dee652e1d30af73ec
Vimal Raval <ravalvimal2008@gmail.com>
2026-08-04 15:11:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (78.86%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -4,22 +4,4 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
-        j = ''
-        if len(s) == 1:
-            return 1
-        elif s.endswith(" ") == False:
-            for i in range(1, len(s) + 1):
-                if s[-i] != " ":
-                    j += s[-i]
-                else:
-                    break
-            return len(j)
-        else:
-            new = s.rstrip()
-            for i in range(1, len(new) + 1):
-                if new[-i] != " ":
-                    j += new[-i]
-                else:
-                    break
-            return len(j)
+        return len(s.split()[-1])
