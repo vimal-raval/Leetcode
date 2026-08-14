f5350dd94a1097f7ea96c5af8d48af2463cbc190
Vimal Raval <ravalvimal2008@gmail.com>
2026-08-14 18:55:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (52.73%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -4,4 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return (nums.index(max(nums)))
+        length = len(nums)
+
+        for i in range(length - 1):
+            if nums[i] > nums[i + 1]:
+                return i
+            
+        return length - 1
